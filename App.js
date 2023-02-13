0e2310ea832e39b67d3bb8c85f8d9d27ce14e69a
Farid <faridqasimov8@gmail.com>
2023-02-13 12:51:02 +0400

    animation

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import LottieView from 'lottie-react-native'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <LottieView source={require('./assets/133564-typing.json')} autoPlay loop/>
     </View>
   );
 }
@@ -13,7 +13,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
